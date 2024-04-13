5dc8554f365f5fe63e7391d6c1dbd63b2256a385
surya382 <suryawanshirahul382@gmail.com>
2024-04-14 00:00:38 +0530

    check status

---
+++ status/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Hello world idffdogd</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
